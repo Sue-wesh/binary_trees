dd3e0756e8648d4dad4318fadfd51ebfbee239e9
Sue-wesh <shiroweru007@gmail.com>
2023-03-02 23:20:16 -0800

    lowest common ancestor

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - find lowest common ancestor of two nodes
+ * @first: pointer to the first node
+ * @second: pointer to the second node
+ * Return: pointer to lowest common ancestor node of the two nodes
+ * NULL if no common ancestor is found
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	binary_tree_t first_par = first->parent;
+	binary_tree_t second_par second->parent;
+
+	if (first || second)
+	{
+		if (first_par == second_par)
+			return (first_par);
+		if (first_par->parent = second_par->parent)
+			return (second_par->parent);
+	}
+	return (NULL);
+}
