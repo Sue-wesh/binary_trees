0a5bd749aea18ea81c4985b0e71eebadd3075e09
Sue-wesh <shiroweru007@gmail.com>
2023-03-02 20:19:41 -0800

    is perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - check if binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ * Return: 0 if tree is NULL
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		if (tree->left && tree->right)
+			return (1);
+	}
+	return (0);
+}
